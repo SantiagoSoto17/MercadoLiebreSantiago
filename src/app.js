c232c4913553df216c1b61796dc476279816f96f
SantiagoSoto17 <santicruzsoto1704@gmail.com>
2023-08-22 00:44:12 -0300

    Primera Maqueta MercadoLiebre

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "../public");
+
+app.use(express.static(publicPath));
+
+const PORT= process.env.PORT || 3000;
+app.listen(PORT, ()=>
+console.log(`servidor corriendo ${PORT}`)
+);
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+  });
+  
+  app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+  });
